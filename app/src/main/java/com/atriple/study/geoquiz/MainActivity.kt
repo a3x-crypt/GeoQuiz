a94c341a5cdb22f001e0aa20487a9c515bda8f28
atriple <andiaqil.dpe@gmail.com>
2020-01-20 14:49:52 +0700

    Add logs on lifecycle callback, activity_main on landscape
    Trying new commit message format. This commit override all lifecycle
    callback, so it will print logs that you can trace it on Logcat so you
    can learn how Activity Lifecycle works.
    
    It also add landscape alternative for activity_main layout. In this
    layout, I use FrameLayout which is the simplest ViewGroup, most of other
    ViewGroup (which is child of FrameLayout) will arrange its View in cert-
    ain way, while FrameLayout don't. You have to configure each View on
    how its going to be flow in the layout (using layout_gravity attr.)

--- app/src/main/java/com/atriple/study/geoquiz/MainActivity.kt
+++ app/src/main/java/com/atriple/study/geoquiz/MainActivity.kt
@@ -2,11 +2,14 @@ package com.atriple.study.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -45,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
-        nextButton.setOnClickListener{
+        nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
 
@@ -56,14 +60,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Challenge Section 2
-        questionTextView.setOnClickListener{
+        questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
 
         //Challenge Section 2
-        prevButton.setOnClickListener{
-            currentIndex = if(currentIndex == 0) {
+        prevButton.setOnClickListener {
+            currentIndex = if (currentIndex == 0) {
                 questionBank.size - 1
             } else currentIndex - 1
             updateQuestion()
@@ -71,18 +75,44 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer: Boolean){
+    private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
 
-        val messageResId = if(userAnswer == correctAnswer){
+        val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
         }
-        else { R.string.incorrect_toast }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
